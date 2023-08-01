b400171aa84087350d16f57f5189eeaffa142b4e
git-user-cpp <akushyk799@gmail.com>
2023-08-01 17:46:24 +0300

    basic structure

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Navbar from './navbar/Navbar.jsx'
+import Header from './header/Header.jsx'
+import Footer from './footer/Footer.jsx'
+
+export {
+    Navbar,
+    Header,
+    Footer,
+}
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from './home/Home.jsx'
+import Blog from './blog/Blog.jsx'
+import Register from './register/Register.jsx'
+
+export {
+    Home,
+    Blog,
+    Register,
+}
